95071613a8296f2b2969a658dd937aeb0658b86e
jarek-prz <j.przyborowski.gm@gmail.com>
2019-10-21 20:32:45 +0200

    Zmiany w skryptach.

--- napisy.py
+++ napisy.py
@@ -6,6 +6,24 @@ napis2='Ala ma kota'
 napis3='A to jest "cudzysłów"'
 napis4 = "A to jest \"cudzysłów\""
 napis5 = "Znaki specjalne: \t, \n, \r, "
-print(napis4)
+#print(napis4)
 dlugosc = len(napis1)
-print("Dlugosc zmiennej napis 1 to: ", dlugosc, " znaków.")
+#print("Dlugosc zmiennej napis 1 to: ", dlugosc, " znaków.")
+
+#wiek = input("Podaj wiek")
+#print("Twój wiek to ", wiek.strip())
+s="Ruda tańczy jak Szalona"
+# metody na rzecz zmiennesj string s
+print(s.capitalize(), s.upper(), s.lower(), s.title(), s.swapcase())
+print(s.center(100))
+print(s.replace("R","D"))
+print("Czy liczba? ", s.isdecimal())
+
+h = "Hello!"
+print(h[0])
+print(h[-1])
+
+print("Litera przedostatnia: ", s[-2])
+
+
+
---
+++ string-slice.py
@@ -0,0 +1,27 @@
+#
+# Array slicing
+#
+
+
+# slice'ing [X:Y]  X-początkowy indeks, Y: końcowy indeks, ale prawostronnie otwarty, czyli bez Y <X:Y)
+h = "Hello!"
+print(h[0:5])
+
+# trzeci znak od końca
+print(h[-3:-1])
+
+s="Ruda tańczy jak szalona"
+# parsowanie, np podział napisu np wg spacji
+arr = s.split(" ")
+print(arr)
+print(arr[1])
+
+# od 1 do 4 elementu listy, ale co drugi
+print(s[0:15:2])
+
+# cały string co 3 znak
+print(s[0::3])
+print(s[::3])
+
+# reverse string w pythonie
+print(s[::-1])
