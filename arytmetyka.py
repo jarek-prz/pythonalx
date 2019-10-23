ab5a8670bbc97b40be52b15764c09057c8209a27
jarek-prz <j.przyborowski.gm@gmail.com>
2019-10-23 18:21:56 +0200

    Dokończenie konfiguracji git

---
+++ arytmetyka.py
@@ -0,0 +1,16 @@
+# przypisz do zmiennych x i y wartości liczbowe korzystając
+# korzystając z print wypisz podstawowe działąnia arytmetyczne +, -, / , *, dzielenie całk, reszta z dziel, potęgowanie
+# uruchom program kilka razy dla róznych wart x i y 
+#
+# automatyczne formatowanie  crl+ alt + l
+
+x = 10
+y = 3
+print("x=", x, "y=", y)
+print("Dodawanie: ", x + y)
+print("Odejmowanie: ", x - y)
+print("Mnożenie: ", x * y)
+print("Dzielenie: ", x / y)
+print("Dzielenie całkowite: ", x // y)
+print("Reszta z dzielenia: ", x % y)
+print("Potęgowanie: ", x ** y)
---
+++ typy_liczbowe.py
@@ -0,0 +1,25 @@
+# w Pythonie mamy:
+# liczby całkowite - integer
+print(type(999))
+
+# zamiana na liczbę
+print(int("10"))
+
+print(int("10", base=10))
+
+# liczba zmiennoprzecinkowa
+print(float())
+print(float(10/3))
+## notacja naukowa
+print(1e12)
+
+# liczby zespolone complex
+print(complex())
+print(10+11j)
+
+# wartości bool: True, False
+print(bool())
+print(10+True)
+# zadanie domowe
+print(not True)
+
